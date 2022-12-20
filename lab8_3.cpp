0d6dc4c6f3393ff2f58dfbe776992955c278596c
tanyongrw <rudjaya22934@gmail.com>
2022-12-20 16:40:35 +0700

    finish

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,19 +1,22 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+char before(char x)
+{
+    if(x <= 'Z' and x >= 'A')
+    {
+        if(x=='A')
+    {
+        x = 'Z';
+    }
+    else
+    {
+        x-- ;
+    }
+    }
+    else
+    {
+        x = '0' ;
+    }
+    return x ;
 }
